3c6a97c0870dd2125c0fb9be177bd6b7b662b6aa
sean <seanlutz@outlook.com>
2018-08-21 20:21:21 -0700

    example

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import requests
+import sys
+app = Flask(__name__)
+def p(x):
+    print(x, file=sys.stderr)
+    
+@app.route("/")
+def health():
+    return "up"
+    
+@app.route('/<path:subpath>')
+def browse(subpath):
+    p("going yay!!")
+    p(subpath)
+    res = requests.get(subpath).text
+    p(type(res))
+    return res
